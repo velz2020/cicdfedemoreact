6e8ba7a799c8fe3de730d33aef7c2ebb4acbfde7
velz <velmurugan.s@venbainfotech.com>
2026-09-19 02:26:57 +0530

    github properly configured

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started with react final pipeline</h1>
+          <h1>Get started with react final pipeline auto</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
